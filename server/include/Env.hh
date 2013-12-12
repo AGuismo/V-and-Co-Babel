7f0d74d5713ec29182402923a0676102378eb58e
lamber-k <kevin.lambert@epitech.eu>
2013-12-12 15:47:41 +0100

    env + path ok

--- server/include/Env.hh
+++ server/include/Env.hh
@@ -2,21 +2,52 @@
 # define ENV_H_
 
 # include	<string>
+# include	<list>
+# include	"IniParser.hh"
 
 class Env
 {
 public:
-  struct
+  typedef std::string		plugin_name;
+  typedef std::string		plugin_path;
+  typedef std::string		file_path;
+  typedef std::string		file_extension;
+
+  struct	PluginDetail
   {
+    PluginDetail(const plugin_name &name, const plugin_path path, bool used = false);
+
+    plugin_name	name;
+    plugin_path	path;
+    bool	used;
+  };
+
+  typedef std::list<PluginDetail>	plugin_list;
+
+  struct				server
+  {
+    static const unsigned short int	PORT     =	44201;
+    static const file_path		CONF_PATH;
+
     unsigned short int			Port;
-    std::string				DatabasePath;
-    std::string				LibraryPath;
-    std::string				confPath;
+    file_path				confPath;
   }					server;
-  static const unsigned short int	PORT     =	44201;
-  static const std::string		DB_PATH;
-  static const std::string		LIBRARY_PATH;
-  static const std::string		CONF_PATH;
+
+  struct				database
+  {
+    static const file_path		DB_PATH;
+
+    file_path				DatabasePath;
+  }					database;
+
+  struct				plugin
+  {
+    static const file_path		LIBRARY_PATH;
+    static const file_extension		LIBRARY_EXTENSION;
+
+    file_path				LibraryPath;
+    plugin_list				plugins;
+  }					plugin;
 
 public:
   static Env	&getInstance();
@@ -30,6 +61,14 @@ private:
   virtual ~Env();
   Env	&operator=(const Env &);
   Env(const Env &);
+
+private:
+  bool			loadPlugins(const parser::Ini::section_key_val &);
+  template <typename T>
+  void			set(T &, const parser::Ini::section &,
+			    const parser::Ini::key &, const parser::Ini::section_key_val &);
+  bool			isPluginSection(const parser::Ini::section &, const parser::Ini::key_val &);
+  const file_path	resolveFilePath(const file_path &);
 };
 
 
